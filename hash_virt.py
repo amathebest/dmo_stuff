635da417eeffd7e7cc8f68c06031bc1d88c1dd28
amathebest <amatori.matt@gmail.com>
2019-11-08 18:18:52 +0100

    Still working on virtual, need to find a way to iter til the right hash is found

--- hash_virt.py
+++ hash_virt.py
@@ -13,7 +13,7 @@ class Page:
         self.overflow = []
 
     def __str__(self):
-        out = "Page " + str(self.index) + ", b: " + str(self.bitmap) + " [" + ",".join(map(str, self.keys)) + "]"
+        out = "Page " + str(self.index) + ", b: " + str(self.bitmap) + ", keys: [" + ",".join(map(str, self.keys)) + "]"
         return out
 
 # function that returns the module of the whole division between the value and the d-power of 2
@@ -27,9 +27,10 @@ else:
     max_len = int(sys.argv[1])  # max lenght of the pages
 d = 1                           # initial number of pages
 pages = []                      # array that will contain the pages
+n_pages = 2                     # int that tracks the total number of pages
 
 # initial allocation of d pages
-for i in range(0, d + 1):
+for i in range(0, n_pages):
     page = Page(i)
     pages.append(page)
 
@@ -42,16 +43,43 @@ while True:
     value = int(value)
     hash_val = hash_fun(value, d)
 
+    reached = False
+    while not reached:
+        if pages[hash_val].bitmap:
+
+
+
+    if len(pages[hash_val].keys) < max_len:                             # non-overflow code
+        pages[hash_val].keys.append(value)
+    else:                                                               # overflow code
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     if len(pages[hash_val].keys) < max_len:                             # non-overflow code
         pages[hash_val].keys.append(value)
     else:                                                               # overflow code
         d = d + 1
+        n_pages = n_pages * 2
         pages[hash_val].overflow.append(value)
         for i in range(d, 2*d):
             new_page = Page(i)
             pages.append(new_page)
         all_keys = pages[hash_val].keys + pages[hash_val].overflow      # defining all the keys to re-map
-        print(all_keys)
+
         for key in all_keys:
             old_hash = hash_fun(key, d-1)
             new_hash = hash_fun(key, d)
@@ -60,9 +88,16 @@ while True:
                     pages[new_hash].keys.append(key)
                 else:
                     pages[new_hash].overflow.append(key)
-                if key in pages[new_hash].keys:
-                    pages[new_hash].keys.remove(key)
+                if key in pages[old_hash].keys:
+                    pages[old_hash].keys.remove(key)
                 else:
+                    pages[old_hash].overflow.remove(key)
+            else:
+                if key in pages[new_hash].overflow:
+                    if len(pages[new_hash].keys) < max_len:
+                        pages[new_hash].keys.append(key)
+                    else:
+                        pages[new_hash].overflow.append(key)
                     pages[new_hash].overflow.remove(key)
 
     print("Current value of d = " + str(d))
