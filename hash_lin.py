fde74e9e25c16caeb3be6cdd241d76e8df2b1bd9
amathebest <amatori.matt@gmail.com>
2019-11-08 13:25:19 +0100

    Fixed mod calculation, still need to figure out why overflow doesn't proc

--- hash_lin.py
+++ hash_lin.py
@@ -12,21 +12,17 @@ class Page:
         self.bitmap = False
         self.overflow = []
 
-    def __str__():
-        return "Page " + str(self.index) + ": [" + ",".join(map(str, self.keys))+ "] " + str(self.bitmap)
+    def __str__(self):
+        return ("Page " + str(self.index) + ": [" + ",".join(map(str, self.keys))+ "] " + str(self.bitmap))
 
-    def length():
-        return len(self.keys)
-
+# function that returns the module of the whole division between the value and the d-power of 2
 def hash_fun(value, d):
-    hash_value = value % pow(2,d)
-    return hash_value
-
+    return value % pow(2, d)
 
 
 # initial parameters
 if len(sys.argv) < 3:
-    d = 2
+    d = 1
     max_len = 2
 else:
     d = sys.argv[1]         # initial number of pages
@@ -37,7 +33,7 @@ bitmap = []                 # array that stores if the page has already been spl
 pages = []                  # array that will contain the pages
 
 # initial allocation of d pages
-for i in range(0, d):
+for i in range(0, d+1):
     page = Page(i)
     pages.append(page)
 
@@ -47,9 +43,9 @@ while True:
     if value == "e":
         print("Exiting program..")
         break
-
+    value = int(value)
     hash_val = hash_fun(value, d)
-    if pages[hash_val].length() != max_len:
+    if len(pages[hash_val].keys) != max_len:
         pages[hash_val].keys.append(value)
     else:
         pages[hash_val].overflow.append(value)
@@ -58,12 +54,12 @@ while True:
         all_keys = pages[p].keys + pages[p].overflow
         for key in all_keys:
             new_hash = hash_fun(key, d+1)
-            if pages[p].length() != max_len:
-                pages[p].keys.append(key)
+            if len(pages[new_hash].keys) != max_len:
+                pages[new_hash].keys.append(key)
             else:
-                pages[p].overflow.append(key)
+                pages[new_hash].overflow.append(key)
         p = p+1
-        if p == pow(2,d):
+        if p == pow(2, d):
             p = 0
             d = d+1
 
