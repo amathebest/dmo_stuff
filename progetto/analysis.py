a5e7ec78ecc7fadd17fe26cdccab13f6cf6874cf
amathebest <amatori.matt@gmail.com>
2019-12-29 12:46:00 +0100

    Moving the dataset into a data folder

--- progetto/analysis.py
+++ progetto/analysis.py
@@ -6,7 +6,7 @@ from modules import Node, Edge
 from modules import bool_math, compute_gini_index, compute_gini_impurity
 
 # data input and initialization
-file_path = "C:/Users/Matteo/Dropbox/University/11/DMO/dmo_stuff/progetto/dataset.csv"
+file_path = "C:/Users/Matteo/Dropbox/University/11/DMO/dmo_stuff/progetto/data/dataset.csv"
 df = pd.read_csv(file_path, sep = ";")
 df.head()
 
